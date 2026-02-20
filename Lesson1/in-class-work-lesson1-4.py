0fab58f7fdf007b5d25dd6e03956613099b19b27
Muhammed Enes Ugurlu <enesugurlu1684@gmail.com>
2026-02-20 11:19:02 +0300

    dördüncü alıştırma yapıldı.

---
+++ Lesson1/in-class-work-lesson1-4.py
@@ -0,0 +1,9 @@
+miles_driven = float(input("Please enter the miles driven: "))
+
+gallon_per_mile = float(input("Please enter the gallon per mile: "))
+
+used_gallon = miles_driven / gallon_per_mile
+
+print(f"Used Gallon: {used_gallon}")
+
+
